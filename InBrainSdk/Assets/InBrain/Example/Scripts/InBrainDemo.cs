441d83d601b08467556cef67617311699e873301
Anastasiia Havryliv <gavrylivnastya@gmail.com>
2026-05-14 15:02:51 +0300

    update inbrain demo scene

--- InBrainSdk/Assets/InBrain/Example/Scripts/InBrainDemo.cs
+++ InBrainSdk/Assets/InBrain/Example/Scripts/InBrainDemo.cs
@@ -43,10 +43,22 @@ namespace InBrain
 			});
 		}
 
+		public void OnOpenWallClicked()
+		{
+			Debug.Log("InBrain: OpenWall (All) button clicked");
+			InBrain.Instance.ShowSurveys();
+		}
+
 		public void OnShowSurveysClicked()
 		{
-			Debug.Log("InBrain: ShowSurveys button clicked");
-			InBrain.Instance.ShowSurveys();
+			Debug.Log("InBrain: OpenWall (Surveys) button clicked");
+			InBrain.Instance.ShowSurveys(InBrainWallOption.SURVEYS);
+		}
+
+		public void OnShowOffersClicked()
+		{
+			Debug.Log("InBrain: OpenWall (Offers) button clicked");
+			InBrain.Instance.ShowSurveys(InBrainWallOption.OFFERS);
 		}
 
 		public void OnShowSurveysListClicked()
